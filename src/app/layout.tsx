a4fc5e2e9789f70ed2c63af0f3619ae11498532f
Edwin Lee <edwinlee107@hotmail.my>
2023-09-30 03:33:12 +0800

    Disable zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,8 @@ export const metadata: Metadata = {
   viewport: {
     width: "device-width",
     initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
     viewportFit: "cover"
   }
 }
