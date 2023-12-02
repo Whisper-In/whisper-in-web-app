de0793c7b9dc4dbc473b22f1c89e18605bbc4418
Edwin Lee <edwinlee107@hotmail.my>
2023-12-02 17:14:11 +0800

    Test user agent

--- src/utils/mobile.util.ts
+++ src/utils/mobile.util.ts
@@ -1,3 +1,6 @@
 const mobileRegex = new RegExp(/Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/gi);
 
-export const isMobile = (userAgent: string | null) => userAgent?.match(mobileRegex)?.length ?? 0 > 0;
+export const isMobile = (userAgent: string | null) => {
+    console.log("userAgent:", userAgent);
+    return userAgent?.match(mobileRegex)?.length ?? 0 > 0
+}
